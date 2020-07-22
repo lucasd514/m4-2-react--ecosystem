fa019986d4048415172cbbd7d07186db2160eea8
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-22 09:10:31 -0400

    CSS

--- src/components/Listinggrid.js
+++ src/components/Listinggrid.js
@@ -2,14 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+const Container = styled.div`
+  width: 1200px;
+`;
+
+const Picture = styled.img`
+  height: 250px;
+  width: 250px;
+`;
+
+const Fruit = styled.div`
+  width: 275;
+  display: inline-block;
+  padding: 50px;
+`;
 const ListingGrid = ({ itemList }) => {
-  return itemList.map((fruits) => (
-    <li>
-      <img src={fruits.imageSrc} alt={fruits.name} />
-      <h2>{fruits.name}</h2>
-      <h2>{fruits.latinname}</h2>
-    </li>
-  ));
+  return (
+    <Container>
+      {itemList.map((fruits) => (
+        <Fruit>
+          <Picture src={fruits.imageSrc} alt={fruits.name} />
+          <div>{fruits.name}</div>
+          <div>{fruits.latinName}</div>
+        </Fruit>
+      ))}
+    </Container>
+  );
 };
 
 export default ListingGrid;
